265c8bd3d4180d6353f69516dccbac3c67761cef
Lina LTU-Ubuntu <lina.van.brugge@tum.de>
2026-08-25 11:21:05 +0200

    add force sensor message + drill motor type

--- include/motor_types.hpp
+++ include/motor_types.hpp
@@ -2,17 +2,19 @@
 #define MOTOR_TYPES_HPP_
 
 enum motorType {
-    BAM_PAD,
-    FAM_PAD,
-    PUSHER_BPM,
-    PUSHER_DPM,
-    AUGER_PIPE
+    BAM_PAD = 0,
+    FAM_PAD = 1,
+    PUSHER_BPM = 2,
+    PUSHER_DPM = 3,
+    AUGER_PIPE = 7,
+    DRILL = 4
 };
 
 enum motorPos {
     LEFT = 10,
     RIGHT = 11,
-    TOP = 12
+    TOP = 12,
+    NONE = 13
 };
 
 #endif
